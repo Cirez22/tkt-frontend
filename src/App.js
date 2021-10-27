915b33806c995f12bd2f2901ea610741b9d09461
unknown <pancorvomatias@gmail.com>
2021-10-27 15:35:10 -0300

    adding dashboard

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Login from "./views/Login";
 import Register from "./views/Register";
 import HomeView from "./views/HomeView";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 
 function App() {
@@ -12,6 +13,9 @@ function App() {
         <Route exact path="/" component={HomeView} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
+        
+        <Route exact path="/dashboard" component={Dashboard} />
+
       </BrowserRouter>
 
     </div>
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Navbar from "./Navbar";
+import Sidebar from "./Sidebar";
+
 
 function Dashboard () {
     return ( 
         <div>
-            
+            <Navbar/>
+            <Sidebar/>
         </div>
     )
 }
